220a62b2705bb519a974154708a33d7a82f3a14b
Rishindu Yohan <rishinduyohan@gmail.com>
2025-10-10 20:04:40 +0530

    Create Login form and controller

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -9,7 +9,7 @@ public class Starter extends Application {
     }
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource(""))));
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Login_form.fxml"))));
         stage.setTitle("Login Form");
         stage.show();
     }
