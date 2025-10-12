ef3f69ab407e91f513e0d780d1fef9a24a2dd072
Rishindu Yohan <rishinduyohan@gmail.com>
2025-10-12 23:30:44 +0530

    Add functions to customer controller

--- src/main/java/controller/CustomerController.java
+++ src/main/java/controller/CustomerController.java
@@ -5,8 +5,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CustomerController {
     Stage stage = new Stage();
@@ -14,8 +19,83 @@ public class CustomerController {
     private AnchorPane mainContent;
 
     @FXML
-    void btnCustomersOnAction(ActionEvent event) {
+    private TableColumn<?, ?> tblAddress;
 
+    @FXML
+    private TableColumn<?, ?> tblCity;
+
+    @FXML
+    private TableColumn<?, ?> tblCusId;
+
+    @FXML
+    private TableView<?> tblCustomer;
+
+    @FXML
+    private TableColumn<?, ?> tblDob;
+
+    @FXML
+    private TableColumn<?, ?> tblName;
+
+    @FXML
+    private TableColumn<?, ?> tblProvince;
+
+    @FXML
+    private TableColumn<?, ?> tblPsCode;
+
+    @FXML
+    private TableColumn<?, ?> tblSalary;
+
+    @FXML
+    private TableColumn<?, ?> tblTitle;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtCity;
+
+    @FXML
+    private TextField txtCusId;
+
+    @FXML
+    private TextField txtDob;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPostalCode;
+
+    @FXML
+    private TextField txtProvince;
+
+    @FXML
+    private TextField txtSalary;
+
+    @FXML
+    private TextField txtTitle;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCustomersOnAction(ActionEvent event) {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/CustomerMgt.fxml"))));
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            currentStage.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.setTitle("Customer Management");
+        stage.show();
     }
 
     @FXML
@@ -32,6 +112,11 @@ public class CustomerController {
     }
 
     @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
     void btnEmployeesOnAction(ActionEvent event) {
 
     }
@@ -43,11 +128,24 @@ public class CustomerController {
 
     @FXML
     void btnLogoutOnAction(ActionEvent event) {
-
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Login_form.fxml"))));
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            currentStage.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.setTitle("Login Form");
+        stage.show();
     }
 
     @FXML
     void btnSuppliersOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
 
     }
 
