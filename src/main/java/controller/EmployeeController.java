0675458a2cd23838a702472e3217d4112743d18e
Rishindu Yohan <rishinduyohan@gmail.com>
2025-10-14 22:05:46 +0530

    Create EmployeeDTO and observerArray

--- src/main/java/controller/EmployeeController.java
+++ src/main/java/controller/EmployeeController.java
@@ -1,8 +1,11 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
@@ -10,11 +13,16 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import model.EmployeeDTO;
 
 import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
 
-public class EmployeeController {
+public class EmployeeController implements Initializable {
     Stage stage = new Stage();
     @FXML
     private TableColumn<?, ?> colAddress;
@@ -44,7 +52,7 @@ public class EmployeeController {
     private TableColumn<?, ?> colStatus;
 
     @FXML
-    private TableColumn<?, ?> colTitle;
+    private TableColumn<?, ?> colName;
 
     @FXML
     private ComboBox<String> comboStatus;
@@ -56,7 +64,7 @@ public class EmployeeController {
     private DatePicker dateJoined;
 
     @FXML
-    private TableView<?> tblEmployee;
+    private TableView<EmployeeDTO> tblEmployee;
 
     @FXML
     private TextField txtAddress;
@@ -79,6 +87,16 @@ public class EmployeeController {
     @FXML
     private TextField txtSalary;
 
+    ObservableList<EmployeeDTO> employeeDTOS = FXCollections.observableArrayList(
+            new EmployeeDTO("E001", "Ravi Perera", "901234567V", "1990-05-12", "Manager", 85000.00, "0771234567", "Colombo", "2015-03-01", "Active"),
+            new EmployeeDTO("E002", "Nimali Silva", "911234568V", "1991-08-22", "Software Engineer", 95000.00, "0772345678", "Kandy", "2018-07-15", "Active"),
+            new EmployeeDTO("E003", "Tharindu Jayasena", "921234569V", "1992-11-05", "HR Executive", 65000.00, "0773456789", "Galle", "2016-01-20", "Inactive"),
+            new EmployeeDTO("E004", "Ishara Fernando", "931234570V", "1993-02-18", "Accountant", 72000.00, "0774567890", "Matara", "2019-09-10", "Active"),
+            new EmployeeDTO("E005", "Kasun Rathnayake", "941234571V", "1994-06-30", "Graphic Designer", 68000.00, "0775678901", "Kurunegala", "2020-05-25", "Active"),
+            new EmployeeDTO("E006", "Dilani Wickramasinghe", "951234572V", "1995-12-12", "Marketing Lead", 80000.00, "0776789012", "Negombo", "2017-11-03", "Inactive"),
+            new EmployeeDTO("E007", "Chamika Bandara", "961234573V", "1996-03-27", "QA Analyst", 70000.00, "0777890123", "Anuradhapura", "2021-02-14", "Active"),
+            new EmployeeDTO("E008", "Sanduni Herath", "971234574V", "1997-09-09", "Intern", 40000.00, "0778901234", "Badulla", "2023-06-01", "Active")
+    );
     @FXML
     void btnAddOnAction(ActionEvent event) {
 
@@ -167,5 +185,28 @@ public class EmployeeController {
     @FXML
     void btnUpdateOnAction(ActionEvent event) {
 
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        colEmpId.setCellValueFactory(new PropertyValueFactory<>("employeeId"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colNic.setCellValueFactory(new PropertyValueFactory<>("nic"));
+        colDob.setCellValueFactory(new PropertyValueFactory<>("dob"));
+        colPosition.setCellValueFactory(new PropertyValueFactory<>("position"));
+        colSalary.setCellValueFactory(new PropertyValueFactory<>("salary"));
+        colContact.setCellValueFactory(new PropertyValueFactory<>("contactNumber"));
+        colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+        colJoined.setCellValueFactory(new PropertyValueFactory<>("joinedDate"));
+        colStatus.setCellValueFactory(new PropertyValueFactory<>("status"));
+        tblEmployee.setItems(employeeDTOS);
+
+        tblEmployee.getSelectionModel().selectedItemProperty().addListener((observable,oldValue,newValue)->{
+            if(null!=newValue){
+                txtEmpId.setText(newValue.getEmployeeId());
+                txtName.setText(newValue.getName());
+                txtNic.setText(newValue.getNic());
+            }
+        });
     }
 }
--- src/main/java/model/EmployeeDTO.java
+++ src/main/java/model/EmployeeDTO.java
@@ -1,4 +1,21 @@
 package model;
 
+import lombok.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
 public class EmployeeDTO {
+    private String employeeId;
+    private String name;
+    private String nic;
+    private String dob;
+    private String position;
+    private double salary;
+    private String contactNumber;
+    private String address;
+    private String joinedDate;
+    private String status;
 }
