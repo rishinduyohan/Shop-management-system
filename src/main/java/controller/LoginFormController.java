51087fb0cb67412c2e7010a9ad222dbcc183290e
Rishindu Yohan <rishinduyohan@gmail.com>
2025-10-28 15:55:27 +0530

    Update login form UI and add enter function to txtFields

--- src/main/java/controller/LoginFormController.java
+++ src/main/java/controller/LoginFormController.java
@@ -28,26 +28,29 @@ public class LoginFormController {
     private TextField txtUsername;
 
     ObservableList<LoginInfoDTO> loginInfoDTOS = FXCollections.observableArrayList(
-            new LoginInfoDTO("Admin",1234),
-            new LoginInfoDTO("Rishindu",2005),
-            new LoginInfoDTO("User",1111)
+            new LoginInfoDTO("Admin", 1234),
+            new LoginInfoDTO("Rishindu", 2005),
+            new LoginInfoDTO("User", 1111)
     );
-    public boolean checkPassword(String username,String password){
-        for(LoginInfoDTO user : loginInfoDTOS){
-            if(username.equals(user.getUsername()) && Integer.parseInt(password)==user.getPassword()){
+
+    public boolean checkPassword(String username, String password) {
+        for (LoginInfoDTO user : loginInfoDTOS) {
+            if (username.equals(user.getUsername()) && Integer.parseInt(password) == user.getPassword()) {
                 return true;
             }
         }
         return false;
     }
-    public void cleanText(){
+
+    public void cleanText() {
         txtUsername.setText("");
         txtPassword.setText("");
     }
+
     @FXML
     void btnLoginOnAction(ActionEvent event) {
-        boolean isUser = checkPassword(txtUsername.getText(),txtPassword.getText());
-        if(isUser){
+        boolean isUser = checkPassword(txtUsername.getText(), txtPassword.getText());
+        if (isUser) {
             try {
                 stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Dashboard.fxml"))));
                 Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -57,7 +60,7 @@ public class LoginFormController {
             }
             stage.setTitle("Dashboard");
             stage.show();
-        }else{
+        } else {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Error");
             alert.setHeaderText("Invalid User");
@@ -69,5 +72,9 @@ public class LoginFormController {
 
     public void txtPasswordOnAction(ActionEvent actionEvent) {
         btnLoginOnAction(actionEvent);
+    }
+
+    public void txtUsernameOnAction(ActionEvent actionEvent) {
+        txtPassword.requestFocus();
     }
 }
